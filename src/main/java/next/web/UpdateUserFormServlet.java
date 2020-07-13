d4c995f0d1e8a42f3f756d857ec7a35f565a91ae
siyoon <siyoon210@gmail.com>
2020-07-13 23:10:12 +0900

    UpdateUserFormServlet 생성

---
+++ src/main/java/next/web/UpdateUserFormServlet.java
@@ -0,0 +1,26 @@
+package next.web;
+
+import core.db.DataBase;
+import next.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/user/update/*")
+public class UpdateUserFormServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private static final Logger log = LoggerFactory.getLogger(UpdateUserFormServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final RequestDispatcher requestDispatcher = req.getRequestDispatcher("/user/update.jsp");
+        requestDispatcher.forward(req, resp);
+    }
+}
