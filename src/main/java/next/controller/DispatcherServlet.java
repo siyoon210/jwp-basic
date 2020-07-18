e66a04bd54eeff667c6cc4534f6dd0865d3b0195
siyoon <siyoon210@gmail.com>
2020-07-18 22:17:24 +0900

    DispatcherServlet 구현

--- src/main/java/next/controller/DispatcherServlet.java
+++ src/main/java/next/controller/DispatcherServlet.java
@@ -1,7 +1,52 @@
 package next.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(name = "dispatcher", urlPatterns = "/", loadOnStartup = 1)
-public class DispatcherServlet {
+public class DispatcherServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
+    private static final String DEFAULT_REDIRECT_PREFIX = "redirect";
+
+    private RequestMapping rm;
+
+    @Override
+    public void init() throws ServletException {
+        rm = new RequestMapping();
+        rm.initMapping();
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final String requestURI = req.getRequestURI();
+        log.debug("Method : {}, Request URI : {}", req.getMethod(), requestURI);
+
+        final Controller controller = rm.findController(requestURI);
+        try {
+            final String viewName = controller.execute(req, resp);
+            move(viewName, req, resp);
+        } catch (Throwable e) {
+            log.error("error: {}", e);
+            throw new ServletException(e.getMessage());
+        }
+    }
+
+    private void move(String viewName, HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        if (viewName.startsWith(DEFAULT_REDIRECT_PREFIX)) {
+            resp.sendRedirect(viewName.substring(DEFAULT_REDIRECT_PREFIX.length()));
+            return;
+        }
+
+        final RequestDispatcher rd = req.getRequestDispatcher(viewName);
+        rd.forward(req, resp);
+    }
 }
