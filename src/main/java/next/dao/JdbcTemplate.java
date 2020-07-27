3d8a48ab6a67eb52a6c7c29a05e2637364f3c9bf
siyoon <siyoon210@gmail.com>
2020-07-28 05:36:28 +0900

    SQLException catch시에 log 추가

--- src/main/java/next/dao/JdbcTemplate.java
+++ src/main/java/next/dao/JdbcTemplate.java
@@ -4,18 +4,23 @@ import core.jdbc.ConnectionManager;
 import next.exception.JdbcTemplateException;
 import next.model.User;
 import org.h2.jdbc.JdbcSQLException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public abstract class JdbcTemplate {
+    private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+
     public void update(String sql) {
         try (Connection con = ConnectionManager.getConnection();
              PreparedStatement pstmt = con.prepareStatement(sql)) {
             this.setValues(pstmt);
             pstmt.executeUpdate();
         } catch (SQLException e) {
+            log.error("SQLException!! {}", e.getMessage());
             throw new JdbcTemplateException();
         }
     }
