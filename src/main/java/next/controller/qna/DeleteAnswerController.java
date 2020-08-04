59420dcea7dccc25072fdaae8f60764199a06518
siyoon <siyoon210@gmail.com>
2020-08-05 08:32:22 +0900

    Controller 반환타입을 View로 변경하고, 구현체 JsonView와 JspView를 반환

--- src/main/java/next/controller/qna/DeleteAnswerController.java
+++ src/main/java/next/controller/qna/DeleteAnswerController.java
@@ -1,28 +1,22 @@
 package next.controller.qna;
 
-import java.io.PrintWriter;
+import core.mvc.Controller;
+import core.mvc.view.JsonView;
+import core.mvc.view.View;
+import next.dao.AnswerDao;
+import next.model.Result;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import core.mvc.Controller;
-import next.dao.AnswerDao;
-import next.model.Result;
-
 public class DeleteAnswerController implements Controller {
     @Override
-    public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+    public View execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         Long answerId = Long.parseLong(req.getParameter("answerId"));
         AnswerDao answerDao = new AnswerDao();
 
         answerDao.delete(answerId);
 
-        ObjectMapper mapper = new ObjectMapper();
-        resp.setContentType("application/json;charset=UTF-8");
-        PrintWriter out = resp.getWriter();
-        out.print(mapper.writeValueAsString(Result.ok()));
-        return null;
+        return new JsonView(Result.ok());
     }
 }
