13b78e583a5d9eed00cf2ff3e77f0a5c0374c442
siyoon <siyoon210@gmail.com>
2020-08-07 20:20:52 +0900

    JspView 수정

--- src/main/java/core/mvc/view/JsonView.java
+++ src/main/java/core/mvc/view/JsonView.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
-import java.util.Enumeration;
 import java.util.Map;
 
 public class JsonView implements View {
@@ -15,15 +14,5 @@ public class JsonView implements View {
         resp.setContentType("application/json;charset=UTF-8");
         PrintWriter out = resp.getWriter();
         out.print(mapper.writeValueAsString(model));
-    }
-
-    private Map<String, Object> createModel(HttpServletRequest req, Map<String, Object> model) {
-        final Enumeration<String> names = req.getAttributeNames();
-        while (names.hasMoreElements()) {
-            final String name = names.nextElement();
-            model.put(name, req.getAttribute(name));
-        }
-
-        return model;
     }
 }
