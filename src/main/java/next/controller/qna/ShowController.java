e222ebd4302864534e754f177c085253ab833b27
siyoon <siyoon210@gmail.com>
2020-08-09 13:58:24 +0900

    9장 7번 ShowController 멀티쓰레드 기반에서 안전하도록 수정

--- src/main/java/next/controller/qna/ShowController.java
+++ src/main/java/next/controller/qna/ShowController.java
@@ -1,29 +1,26 @@
 package next.controller.qna;
 
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import core.mvc.AbstractController;
+import core.mvc.ModelAndView;
 import next.dao.AnswerDao;
 import next.dao.QuestionDao;
 import next.model.Answer;
 import next.model.Question;
-import core.mvc.AbstractController;
-import core.mvc.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 public class ShowController extends AbstractController {
     private QuestionDao questionDao = new QuestionDao();
     private AnswerDao answerDao = new AnswerDao();
-    private Question question;
-    private List<Answer> answers;
 
     @Override
     public ModelAndView execute(HttpServletRequest req, HttpServletResponse response) throws Exception {
         Long questionId = Long.parseLong(req.getParameter("questionId"));
 
-        question = questionDao.findById(questionId);
-        answers = answerDao.findAllByQuestionId(questionId);
+        final Question question = questionDao.findById(questionId);
+        final List<Answer> answers = answerDao.findAllByQuestionId(questionId);
 
         ModelAndView mav = jspView("/qna/show.jsp");
         mav.addObject("question", question);
