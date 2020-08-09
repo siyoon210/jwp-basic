1d25f5e740e0b841f76d9b72068f77eaf01114a3
siyoon <siyoon210@gmail.com>
2020-08-09 13:23:14 +0900

    9장 4번, 로그인하지 않은 경우 질문하기 못하도록 수정

--- src/main/java/next/controller/qna/CreateQuestionController.java
+++ src/main/java/next/controller/qna/CreateQuestionController.java
@@ -10,11 +10,18 @@ import org.slf4j.LoggerFactory;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import static next.controller.UserSessionUtils.isLogined;
+
 public class CreateQuestionController extends AbstractController {
     private static final Logger log = LoggerFactory.getLogger(CreateQuestionController.class);
 
     @Override
     public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        if (!isLogined(request.getSession())) {
+            log.info("Login required.");
+            return jspView("redirect:/users/loginForm");
+        }
+
         Question question = new Question(request);
         QuestionDao questionDao = new QuestionDao();
         question = questionDao.insert(question);
