bd01b39ee21d2672ab73cd39b947e356bf518077
siyoon <siyoon210@gmail.com>
2020-08-11 22:43:49 +0900

    JUnit4 -> JUnit5, Mockito 의존성 추가

--- src/test/java/next/dao/QuestionDaoTest.java
+++ src/test/java/next/dao/QuestionDaoTest.java
@@ -1,20 +1,19 @@
 package next.dao;
 
-import org.junit.Before;
-import org.junit.Test;
+import core.jdbc.ConnectionManager;
+import next.model.Question;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
-import core.jdbc.ConnectionManager;
-import next.model.Question;
-
 public class QuestionDaoTest {
     private static final Logger log = LoggerFactory.getLogger(QuestionDaoTest.class);
     
-    @Before
+    @BeforeEach
     public void setup() {
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
         populator.addScript(new ClassPathResource("jwp.sql"));
