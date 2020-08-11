06ac949a8ebeb033a5c69d432e9f5913d641df8f
siyoon <siyoon210@gmail.com>
2020-08-11 22:29:32 +0900

    9장 13번 QuestionService 생성

---
+++ src/main/java/next/controller/qna/DeleteQuestionController.java
@@ -0,0 +1,35 @@
+package next.controller.qna;
+
+import core.mvc.AbstractController;
+import core.mvc.ModelAndView;
+import next.controller.UserSessionUtils;
+import next.dao.QuestionDao;
+import next.model.User;
+import next.service.QuestionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static next.controller.UserSessionUtils.isLogined;
+
+public class DeleteQuestionController extends AbstractController {
+    private static final Logger log = LoggerFactory.getLogger(DeleteQuestionController.class);
+    private final QuestionDao questionDao = new QuestionDao();
+    private final QuestionService questionService = new QuestionService();
+
+    @Override
+    public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        if (!isLogined(request.getSession())) {
+            log.info("Login required.");
+            return jspView("redirect:/users/loginForm");
+        }
+
+        final long questionId = Long.parseLong(request.getParameter("questionId"));
+        final User userFromSession = UserSessionUtils.getUserFromSession(request.getSession());
+        questionService.deleteQuestion(questionId, userFromSession);
+        log.info("Delete Question. {}", questionId);
+        return jspView("redirect:/");
+    }
+}
---
+++ src/main/java/next/service/QuestionService.java
@@ -0,0 +1,9 @@
+package next.service;
+
+import next.model.User;
+
+public class QuestionService {
+    public long deleteQuestion(long questionId, User loginUser) {
+        return questionId;
+    }
+}
