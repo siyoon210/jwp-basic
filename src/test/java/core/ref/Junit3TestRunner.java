b1c56e7985ccd726b47f0a2988c0874e83c8e30c
siyoon <siyoon210@gmail.com>
2020-08-25 08:25:53 +0900

    양파 10.1.2.2 ("test"로 시작하는 메서드 실행하기)

--- src/test/java/core/ref/Junit3TestRunner.java
+++ src/test/java/core/ref/Junit3TestRunner.java
@@ -2,10 +2,23 @@ package core.ref;
 
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class Junit3TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-
+        final Method[] methods = clazz.getMethods();
+        Arrays.stream(methods)
+                .filter(m -> m.getName().startsWith("test"))
+                .forEach(m -> {
+                    try {
+                        m.invoke(clazz.getDeclaredConstructor().newInstance());
+                    } catch (IllegalAccessException | InvocationTargetException | InstantiationException | NoSuchMethodException e) {
+                        e.printStackTrace();
+                    }
+                });
     }
 }
