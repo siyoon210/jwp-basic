dacd9d11c90dea869a6e9f02e18ea850efb4d723
siyoon <siyoon210@gmail.com>
2020-08-25 08:18:25 +0900

    양파 10.1.2.1 (자바 리플렉션 API 활용해 클래스 정보 출력하기)

--- src/test/java/core/ref/ReflectionTest.java
+++ src/test/java/core/ref/ReflectionTest.java
@@ -1,11 +1,15 @@
 package core.ref;
 
+import next.model.Question;
+import next.model.User;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import next.model.Question;
-import next.model.User;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -13,7 +17,30 @@ public class ReflectionTest {
     @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
-        logger.debug(clazz.getName());
+
+        logger.debug("Print all public FIELDS");
+        final Field[] fields = clazz.getFields();
+        Arrays.stream(fields).forEach(f -> logger.debug(f.getName()));
+
+        logger.debug("Print all public CONSTRUCTORS");
+        final Constructor<?>[] constructors = clazz.getConstructors();
+        Arrays.stream(constructors).forEach(c -> logger.debug(c.getName()));
+
+        logger.debug("Print all public METHODS");
+        final Method[] methods = clazz.getMethods();
+        Arrays.stream(methods).forEach(m -> logger.debug(m.getName()));
+
+        logger.debug("Print all non-public FIELDS");
+        final Field[] declaredFields = clazz.getDeclaredFields();
+        Arrays.stream(declaredFields).forEach(f -> logger.debug(f.getName()));
+
+        logger.debug("Print all non-public CONSTRUCTORS");
+        final Constructor<?>[] declaredConstructors = clazz.getDeclaredConstructors();
+        Arrays.stream(declaredConstructors).forEach(f -> logger.debug(f.getName()));
+
+        logger.debug("Print all non-public METHODS");
+        final Method[] declaredMethods = clazz.getDeclaredMethods();
+        Arrays.stream(declaredMethods).forEach(m -> logger.debug(m.getName()));
     }
     
     @Test
