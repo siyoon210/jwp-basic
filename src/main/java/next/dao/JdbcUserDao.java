20c3c8d448b9485db1936cd1b1a3be06ba93dd8d
siyoon <siyoon210@gmail.com>
2020-09-02 16:09:55 +0900

    feat: UserDao를 인터페이스를 구현하는 JdbcUserDao 생성
    (기존 UserDao 복사)

---
+++ src/main/java/next/dao/JdbcUserDao.java
@@ -0,0 +1,42 @@
+package next.dao;
+
+import core.jdbc.JdbcTemplate;
+import core.jdbc.RowMapper;
+import next.model.User;
+
+import java.util.List;
+
+public class JdbcUserDao implements UserDao {
+    private final JdbcTemplate jdbcTemplate = JdbcTemplate.getInstance();
+
+    public JdbcUserDao() {
+    }
+
+    public void insert(User user) {
+        String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
+        jdbcTemplate.update(sql, user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
+    }
+
+    public User findByUserId(String userId) {
+        String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
+
+        RowMapper<User> rm = rs -> new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
+                rs.getString("email"));
+
+        return jdbcTemplate.queryForObject(sql, rm, userId);
+    }
+
+    public List<User> findAll() {
+        String sql = "SELECT userId, password, name, email FROM USERS";
+
+        RowMapper<User> rm = rs -> new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
+                rs.getString("email"));
+
+        return jdbcTemplate.query(sql, rm);
+    }
+
+    public void update(User user) {
+        String sql = "UPDATE USERS set password = ?, name = ?, email = ? WHERE userId = ?";
+        jdbcTemplate.update(sql, user.getPassword(), user.getName(), user.getEmail(), user.getUserId());
+    }
+}
