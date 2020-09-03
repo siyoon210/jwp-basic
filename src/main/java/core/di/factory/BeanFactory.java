dd4eef34548b191ab8ed67afd5bcfbc00483827c
siyoon <siyoon210@gmail.com>
2020-09-04 07:12:26 +0900

    feat: 재귀적으로 빈타입 클래스 탐색하고 오브젝트 생성하기

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -1,12 +1,13 @@
 package core.di.factory;
 
-import java.util.Map;
-import java.util.Set;
-
+import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.collect.Maps;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+import java.util.Set;
 
 public class BeanFactory {
     private static final Logger logger = LoggerFactory.getLogger(BeanFactory.class);
@@ -25,6 +26,46 @@ public class BeanFactory {
     }
 
     public void initialize() {
-
+        for (Class<?> preInstanticateBean : preInstanticateBeans) {
+            beans.put(preInstanticateBean, createBean(preInstanticateBean));
+        }
     }
+
+    @SuppressWarnings("unchecked")
+    private <T> T createBean(Class<T> beanType) {
+        if (hasBean(beanType)) {
+            getBean(beanType);
+        }
+
+        //기본생성자이거나, @Inject 명시후에 파라미터가 없는경우
+        final Constructor<?> constructor = BeanFactoryUtils.getInjectedConstructor(beanType);
+        if (constructor == null) {
+            try {
+                return beanType.getConstructor().newInstance();
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        // @Inject 명시하고 파라미터가 있는경우
+        final Class<?>[] constructorParameterTypes = constructor.getParameterTypes();
+        final Object[] parameterObjects = new Object[constructorParameterTypes.length];
+        for (int i = 0; i < constructorParameterTypes.length; i++) {
+            parameterObjects[i] = createBean(constructorParameterTypes[i]);
+        }
+
+        try {
+            return (T) constructor.newInstance(parameterObjects);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private <T> boolean hasBean(Class<T> beanType) {
+        return beans.containsKey(beanType);
+    }
+
+
 }
